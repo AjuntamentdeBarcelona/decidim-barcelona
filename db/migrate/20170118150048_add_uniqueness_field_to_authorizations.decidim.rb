08c3e8a830edae68193d913a190b52e6c54df69e
Josep Jaume Rey <josepjaume@gmail.com>
2017-01-18 16:15:49 +0100

    Upgrade decidim (#10)
    * Upgrade decidim
    
    * Ignore migrations

---
+++ db/migrate/20170118150048_add_uniqueness_field_to_authorizations.decidim.rb
@@ -0,0 +1,8 @@
+# This migration comes from decidim (originally 20170117142904)
+class AddUniquenessFieldToAuthorizations < ActiveRecord::Migration[5.0]
+  def change
+    change_table :decidim_authorizations do |t|
+      t.string :unique_id, null: true
+    end
+  end
+end
