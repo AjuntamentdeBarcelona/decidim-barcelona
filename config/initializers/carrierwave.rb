8f4631a9f89ce0bfc3811403dac5c862a8052708
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-01-18 15:18:02 +0100

    Fix region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -18,8 +18,8 @@ if Rails.env.production?
       provider:              "AWS",                                             # required
       aws_access_key_id:     Rails.application.secrets.aws_access_key_id,       # required
       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,   # required
-      host:                  "s3.eu-central-1.amazonaws.com",
-      region:                "eu-central-1"                                     # optional, defaults to 'us-east-1'
+      host:                  "s3-eu-west-1.amazonaws.com",
+      region:                "eu-west-1"                                     # optional, defaults to 'us-east-1'
     }
     config.fog_directory  = "decidim-barcelona"                                 # required
     config.fog_attributes = { "Cache-Control" => "max-age=#{365.day.to_i}" }    # optional, defaults to {}
