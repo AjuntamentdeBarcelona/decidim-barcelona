77a4cd769d9b91c2fcfb9d838cdbd8aa370af424
Genís Matutes Pujol <genis.matutes@gmail.com>
2017-01-25 12:57:12 +0100

    Update decidim

---
+++ db/migrate/20170125115659_add_user_group_id_to_proposals.decidim_proposals.rb
@@ -0,0 +1,6 @@
+# This migration comes from decidim_proposals (originally 20170120151202)
+class AddUserGroupIdToProposals < ActiveRecord::Migration[5.0]
+  def change
+    add_column :decidim_proposals_proposals, :decidim_user_group_id, :integer, index: true
+  end
+end
