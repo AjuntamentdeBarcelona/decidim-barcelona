b0b31b857c5c4e7d54df7d2bdb7e29e7fd930dcc
Genís Matutes Pujol <genis.matutes@gmail.com>
2017-01-25 14:07:04 +0100

    Remove attachments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125115659) do
+ActiveRecord::Schema.define(version: 20170125130555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "attachments", force: :cascade do |t|
-  end
 
   create_table "decidim_admin_participatory_process_user_roles", force: :cascade do |t|
     t.integer  "decidim_user_id"
