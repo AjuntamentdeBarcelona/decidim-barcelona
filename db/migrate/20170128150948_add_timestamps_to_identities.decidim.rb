9e4a66b0f6438375796734b7e29f5e2fc1eb01f6
Oriol Gual <oriol.gual@gmail.com>
2017-01-28 16:05:04 +0100

    Update Decidim

---
+++ db/migrate/20170128150948_add_timestamps_to_identities.decidim.rb
@@ -0,0 +1,6 @@
+# This migration comes from decidim (originally 20170128140553)
+class AddTimestampsToIdentities < ActiveRecord::Migration[5.0]
+  def change
+    add_timestamps :decidim_identities
+  end
+end
