e9255bf6b3d241a0bb7593c79a0ad858826621c7
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-02-06 11:50:18 +0100

    Add redirects

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,39 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
+  get "processes/:process_slug", to: redirect { |params, _request|
+    process = Decidim::ParticipatoryProcess.find_by_slug(params[:process_slug])
+    "/processes/#{process.id}"
+  }, constraints: { process_slug: /[^0-9]+/ }
+
+  feature_translations = {
+    action_plans: [:results, Decidim::Results::Result],
+    meetings: [:meetings, Decidim::Meetings::Meeting],
+    proposals: [:proposals, Decidim::Proposals::Proposal],
+    debates: [:debates, Decidim::Debates::Debate]
+  }
+
+  get "/:process_slug/:step_id/:feature_name/(:resource_id)", to: redirect { |params, _request|
+    process = Decidim::ParticipatoryProcess.find_by_slug(params[:process_slug]) || Decidim::ParticipatoryProcess.find(params[:process_slug])
+
+    feature_translation = feature_translations[params[:feature_name].to_sym]
+    feature_manifest_name = feature_translation[0]
+
+    feature = Decidim::Feature.find_by(
+      manifest_name: feature_manifest_name,
+      participatory_process: process
+    )
+
+    if params[:resource_id]
+      resource_class = feature_translation[1]
+      resource = resource_class.where("extra->>'slug' = ?", params[:resource_id]).first || resource_class.find(params[:resource_id])
+
+      "/processes/#{process.id}/f/#{feature.id}/#{feature_manifest_name}/#{resource.id}"
+    else
+      "/processes/#{process.id}/f/#{feature.id}"
+    end
+  }, constraints: { process_id: /[^0-9]+/, step_id: /[0-9]+/, feature_name: Regexp.new(feature_translations.keys.join("|")) }
+
   authenticate :user, lambda { |u| u.roles.include?("admin") } do
     mount Sidekiq::Web => '/sidekiq'
   end
