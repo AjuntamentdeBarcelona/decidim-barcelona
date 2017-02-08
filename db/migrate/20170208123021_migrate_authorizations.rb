179ebcd915b76ee721f3535e822e88b6f940c916
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-02-08 13:35:06 +0100

    Update schema and add migration

--- db/migrate/20170208123021_migrate_authorizations.rb
+++ db/migrate/20170208123021_migrate_authorizations.rb
@@ -1,13 +1,14 @@
 class MigrateAuthorizations < ActiveRecord::Migration[5.0]
   def change
-    Decidim::Authorization.each do |authorization|
-      scope_id = authorization.metadata["scope_id"]
+    Decidim::Authorization.all.find_each do |authorization|
+      metadata = authorization.metadata
+      scope_id = metadata["scope_id"]
 
       next unless scope_id.present?
 
       authorization.update_attribute(:metadata, {
                                        postal_code: metadata["postal_code"],
-                                       scope: Decidim::Scope.find(scope_id)
+                                       scope: Decidim::Scope.find(scope_id).name
                                      })
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207200626) do
+ActiveRecord::Schema.define(version: 20170208123021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "decidim_admin_participatory_process_user_roles", force: :cascade do |t|
     t.integer  "decidim_user_id"
