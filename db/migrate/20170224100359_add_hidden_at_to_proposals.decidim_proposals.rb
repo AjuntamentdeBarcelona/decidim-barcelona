d19623f737304ade61e189a638221fda60843bbb
Josep Jaume Rey <josepjaume@gmail.com>
2017-02-24 11:42:05 +0100

    Update decidim (#68)

---
+++ db/migrate/20170224100359_add_hidden_at_to_proposals.decidim_proposals.rb
@@ -0,0 +1,6 @@
+# This migration comes from decidim_proposals (originally 20170220152416)
+class AddHiddenAtToProposals < ActiveRecord::Migration[5.0]
+  def change
+    add_column :decidim_proposals_proposals, :hidden_at, :datetime
+  end
+end
