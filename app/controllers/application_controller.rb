de565bf581a822524bd78d5107e323600c6b12d6
Genís Matutes Pujol <genis.matutes@gmail.com>
2017-03-10 15:25:20 +0100

    Update decidim

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def append_info_to_payload(payload)
-    super
-    payload[:remote_ip] = request.remote_ip
-    payload[:referer] = request.referer.to_s
-    payload[:request_id] = request.uuid
-    payload[:user_agent] = request.user_agent
-    payload[:xhr] = request.xhr? ? 'true' : 'false'
-  end
 end
