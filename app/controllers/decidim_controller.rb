d359efedd8595792af5dfe404e225f8eca1d67bf
Genís Matutes Pujol <genis.matutes@gmail.com>
2017-03-15 14:38:52 +0100

    Add Raven context

--- app/controllers/decidim_controller.rb
+++ app/controllers/decidim_controller.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 class DecidimController < ActionController::Base
+  before_action :set_raven_context
+
+  def set_raven_context
+    return unless Rails.env.production?
+    Raven.user_context({ id: session[:current_user_id] }.merge(session))
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
 end
