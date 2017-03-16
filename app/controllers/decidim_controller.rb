1f7af528b84bfd0bcc2609f8f6551790bbcad840
Genís Matutes <genis.matutes@gmail.com>
2017-03-16 09:28:17 +0100

    Add Raven context (#84)
    * Add Raven context
    
    * Add sentry config at rails secrets

--- app/controllers/decidim_controller.rb
+++ app/controllers/decidim_controller.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 class DecidimController < ActionController::Base
+  before_action :set_raven_context
+
+  def set_raven_context
+    return unless Rails.application.secrets.sentry_enabled?
+    Raven.user_context({id: try(:current_user).try(:id)}.merge(session))
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
 end
