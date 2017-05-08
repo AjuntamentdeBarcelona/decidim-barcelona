7a74c4ef41a681debeaf583bbc2cd1de8311766d
Amaia Castro <amaia@amaiac.net>
2017-05-08 15:40:09 +0200

    Update gem decidim-accountability source and install new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170505213302) do
+ActiveRecord::Schema.define(version: 20170508125649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 20170505213302) do
     t.integer  "decidim_feature_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.jsonb    "description"
+    t.integer  "progress"
     t.index ["decidim_feature_id"], name: "index_decidim_accountability_statuses_on_decidim_feature_id", using: :btree
   end
 
