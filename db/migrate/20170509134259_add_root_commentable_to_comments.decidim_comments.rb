e53f57a53b8217956600169a026f49a3d5ea5cf4
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-05-10 11:00:54 +0200

    Remove long migration

--- db/migrate/20170509134259_add_root_commentable_to_comments.decidim_comments.rb
+++ db/migrate/20170509134259_add_root_commentable_to_comments.decidim_comments.rb
@@ -12,14 +12,14 @@ class AddRootCommentableToComments < ActiveRecord::Migration[5.0]
     end
 
     # 2. Store root_commentable data
-    Decidim::Comments::Comment.find_each do |comment|
-      root_commentable = comment.depth.zero? ? comment.commentable : comment.root_commentable
-      comment.root_commentable = root_commentable(comment)
-      comment.save
-    end
+    # Decidim::Comments::Comment.find_each do |comment|
+    #   root_commentable = comment.depth.zero? ? comment.commentable : comment.root_commentable
+    #   comment.root_commentable = root_commentable(comment)
+    #   comment.save
+    # end
 
     # 3. Set root_commentable fields null constraint
-    change_column :decidim_comments_comments, :decidim_root_commentable_id, :integer, null: false
-    change_column :decidim_comments_comments, :decidim_root_commentable_type, :string, null: false
+    # change_column :decidim_comments_comments, :decidim_root_commentable_id, :integer, null: false
+    # change_column :decidim_comments_comments, :decidim_root_commentable_type, :string, null: false
   end
 end
