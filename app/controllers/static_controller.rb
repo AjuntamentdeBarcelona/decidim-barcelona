15966508716d5c30dfd08b82bfbe9f106cf8e22f
Amaia Castro <amaia@amaiac.net>
2017-05-26 10:45:10 +0200

    Change meta tags

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,20 +1,13 @@
 class StaticController < ApplicationController
   include Decidim::LocaleSwitcher
   include Decidim::NeedsOrganization
+  include Decidim::MetaTagsHelper
 
   layout "decidim/application"
-
-  helper_method :decidim_page_title
 
   def accountability
   end
 
   def accountability_sections
-  end
-
-  private
-
-  def decidim_page_title
-    I18n.t("static.accountability.html_head_title")
   end
 end
