29584db56297f644915057840123284f37bcc6e2
Josep Jaume Rey <josepjaume@gmail.com>
2017-06-14 19:30:08 +0200

    Conditionally show accountability depending on the organization (#132)

--- config/initializers/main_navigation.rb
+++
@@ -1,6 +0,0 @@
-Decidim.menu :menu do |menu|
-  menu.item I18n.t("menu.accountability_static"),
-            DecidimBarcelona::Application.routes.url_helpers.accountability_static_path,
-            position: 4,
-            active: :inclusive
-end
