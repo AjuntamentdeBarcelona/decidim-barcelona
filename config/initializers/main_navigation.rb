fb9ed07211967ae8864acbcfb97247bc86f4fb35
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-06-14 19:50:29 +0200

    Conditionally show accountability navigation

--- config/initializers/main_navigation.rb
+++ config/initializers/main_navigation.rb
@@ -1,6 +1,8 @@
 Decidim.menu :menu do |menu|
-  menu.item I18n.t("menu.accountability_static"),
-            DecidimBarcelona::Application.routes.url_helpers.accountability_static_path,
-            position: 4,
-            active: :inclusive
+  if current_organization.host == "www.decidim.barcelona"
+    menu.item I18n.t("menu.accountability_static"),
+              DecidimBarcelona::Application.routes.url_helpers.accountability_static_path,
+              position: 4,
+              active: :inclusive
+  end
 end
