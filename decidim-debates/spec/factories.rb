877e5f1e2fa242ae31ba32b9bbb39d0a95bbbd6d
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-09-15 14:32:13 +0200

    Add missing factories

--- decidim-debates/spec/factories.rb
+++ decidim-debates/spec/factories.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require "decidim/core/test/factories"
 require "decidim/comments/test/factories"
+require "decidim/participatory_processes/test/factories"
 
 FactoryGirl.define do
   factory :debate, class: Decidim::Debates::Debate do
