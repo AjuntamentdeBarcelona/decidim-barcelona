b3d7a0f83f51a8ab10e214a6cd33977d1078dd99
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-09-21 10:18:21 +0200

    Fix menu

--- config/initializers/main_navigation.rb
+++ config/initializers/main_navigation.rb
@@ -1,5 +1,5 @@
 Decidim.menu :menu do |menu|
-  if current_organization.host == "localhost"
+  if current_organization.host == "www.decidim.barcelona"
     menu.item I18n.t("menu.accountability_static"),
               DecidimBarcelona::Application.routes.url_helpers.accountability_static_path,
               position: 4,
