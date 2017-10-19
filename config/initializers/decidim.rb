c4c62de56f81e9dd61716bc6682718d3b2dd6fd2
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-10-19 18:15:33 +0200

    Use strings in authorization handlers

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -4,7 +4,7 @@ require_relative "../../app/services/census_authorization_handler"
 Decidim.configure do |config|
   config.application_name = "Decidim Barcelona"
   config.mailer_sender    = Rails.application.secrets.email
-  config.authorization_handlers = [CensusAuthorizationHandler]
+  config.authorization_handlers = ["CensusAuthorizationHandler"]
   config.maximum_attachment_size = 150.megabytes
 
   config.available_locales = %i(ca es)
