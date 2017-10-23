31f2a39aa2f5e290114e8396b98c84ae226b59ab
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-10-23 15:02:49 +0200

    Revert "Fix migration handlers"
    This reverts commit 61bc610b9bde6efaa9fce6f5a2d8a9bf88a85d1d.

--- db/migrate/20170315082560_add_available_authorizations_to_organization.decidim.rb
+++ db/migrate/20170315082560_add_available_authorizations_to_organization.decidim.rb
@@ -3,7 +3,7 @@ class AddAvailableAuthorizationsToOrganization < ActiveRecord::Migration[5.0]
   def change
     add_column :decidim_organizations, :available_authorizations, :string, array: true, default: []
 
-    handlers = Decidim.authorization_handlers.map(&:constantize)
+    handlers = Decidim.authorization_handlers.map(&:name)
     Decidim::Organization.find_each do |org|
       org.update_attributes(available_authorizations: handlers)
     end
