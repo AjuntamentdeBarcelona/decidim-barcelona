fbabe1b0f49b6d8a2e974fb82c4b1caf3027e5a6
Fernando Blat <ferblape@gmail.com>
2018-01-19 12:57:39 +0100

    Refactor calculation

--- app/models/decidim/accountability/result_with_weighted_progress.rb
+++ app/models/decidim/accountability/result_with_weighted_progress.rb
@@ -8,15 +8,23 @@ module Decidim
 
       belongs_to :parent, foreign_key: "parent_id", class_name: "Decidim::Accountability::ResultWithWeightedProgress", inverse_of: :children, optional: true, counter_cache: :children_count
 
+      # There are two ways to update parent's progress:
+      #   - using weights, in which case each progress is multiplied by the weigth and them summed
+      #   - not using weights, and using the average of progress of each children
       def update_progress!
-        if children.length > 1 && children.pluck(:weight).any? {|weight| weight == 1.0 }
-          # Not using weights
-          self.progress = children.average(:progress)
+        if children_use_weighted_progress?
+          self.progress = children.sum{|result| result.progress.to_f * result.weight }
         else
-          children_sum = children.sum{ |r| (r.progress.to_f*r.weight) / 100.0 }
-          self.progress = children_sum * 100
+          self.progress = children.average(:progress)
         end
         save!
+      end
+
+      private
+
+      # When a row uses weight 1 and there's more than one, weight shouldn't be considered
+      def children_use_weighted_progress?
+        children.length == 1 || children.pluck(:weight).none?{|weight| weight == 1.0 }
       end
     end
   end
