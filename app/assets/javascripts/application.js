5beb7dfa9b2e763262d45e57c5fcfbf7b6d301b7
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2018-03-19 16:34:15 +0100

    Add fake consultations header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require_tree .
 //= require decidim
+//= require_tree .
