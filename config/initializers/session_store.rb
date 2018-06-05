f5e6b96067cd06c2e17bcfe14122ddb6f0f7d1c6
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2018-06-05 11:33:27 +0200

    Use secure cookies on production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_decidim-barcelona_session"
+Rails.application.config.session_store :cookie_store, key: "_decidim-barcelona_session", secure: Rails.env.production?, httponly: true
