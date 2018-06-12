127371d299aee19b3846851ad7328f7a1d91bf0c
Marc Riera Casals <mrc2407@gmail.com>
2018-06-12 11:22:44 +0200

    s/feature/component/

--- decidim-dataviz/lib/decidim/dataviz/feature.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_dependency "decidim/features/namer"
-
-Decidim.register_feature(:dataviz) do |feature|
-  feature.engine = Decidim::Dataviz::Engine
-  feature.icon = "decidim/dataviz/icon.svg"
-  feature.stylesheet = "decidim/dataviz/dataviz"
-end
