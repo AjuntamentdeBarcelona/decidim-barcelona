a9a199403863ac51d60606489d6a9277e9a98c5d
Fernando Blat <ferblape@gmail.com>
2018-06-20 07:39:11 +0200

    Add weight column in the exporter (#214)

---
+++ spec/services/results_csv_exporter_spec.rb
@@ -0,0 +1,30 @@
+# coding: utf-8
+# frozen_string_literal: true
+require "rails_helper"
+require "decidim/core/test/factories"
+require "decidim/accountability/test/factories"
+require "decidim/participatory_processes/test/factories"
+
+describe Decidim::Accountability::ResultsCSVExporter do
+  let(:organization) { create :organization, available_locales: [:en] }
+  let(:current_user) { create :user, organization: organization }
+  let(:participatory_process) { create :participatory_process, organization: organization }
+  let(:current_feature) { create :accountability_feature, participatory_space: participatory_process }
+  let!(:parent_result) { create :result, feature: current_feature, external_id: "pm-act-423" }
+
+  subject { described_class.new(current_feature) }
+
+  describe "#export" do
+    it "should update them" do
+      csv_export = subject.export
+
+      csv = CSV.parse(csv_export, headers: true)
+
+      expect(csv.count).to eq 1
+      first_row = csv[0]
+      expect(first_row["external_id"]).to eq(parent_result.external_id)
+      expect(first_row["relative_weight"].to_f).to eq(parent_result.weight * 100.0)
+      expect(first_row["result_id"].to_i).to eq(parent_result.id)
+    end
+  end
+end
