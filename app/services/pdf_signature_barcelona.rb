df51cffb11e8bb5484c6bb06ecde3d5ea7006a71
Eduardo Martínez <eduardomech@gmail.com>
2019-01-31 14:27:19 +0100

    Pdf signature service (#246)
    * Add origami gem to manipulate pdf files
    
    * Add service to sign pdf files taking credentials from secrets
    
    * Add tests for pdf sugnature Barcelona service

---
+++ app/services/pdf_signature_barcelona.rb
@@ -0,0 +1,141 @@
+# frozen_string_literal: false
+
+require "origami"
+require "tempfile"
+
+# Service to add a signature to a pdf using certificates provided to
+# Barcelona by Consorci AOC
+#
+# There are three values stored in secrets:
+#
+# * A PEM certificate stored in a string
+# * A file path to a file containing a DER encoded PKCS12 string
+# * A password for previous file
+#
+class PdfSignatureBarcelona
+  attr_accessor :pdf
+
+  # Public: Initializes the service.
+  # pdf - The pdf document to be signed
+  def initialize(args = {})
+    @pdf = args.fetch(:pdf)
+  end
+
+  # Public: PDF signed if certificates can be obtained from secrets, the
+  # original pdf otherwise
+  def signed_pdf
+    return pdf if missing_configuration?
+
+    @signed_pdf ||= begin
+                      StringIO.open(pdf) do |stream|
+                        parsed_pdf = Origami::PDF.read(stream)
+                        parsed_pdf.append_page do |page|
+                          page.add_annotation(signature_annotation)
+                          parsed_pdf.sign(
+                            certificate,
+                            private_key.key,
+                            method: "adbe.pkcs7.detached",
+                            annotation: signature_annotation,
+                            location: location,
+                            contact: contact,
+                            issuer: issuer
+                          )
+                        end
+                        extract_signed_pdf(parsed_pdf)
+                      end
+                    end
+  end
+
+  private
+
+  def missing_configuration?
+    [pdf_certificate, der_pkcs12_path, signature_certificate_password].any?(&:blank?)
+  end
+
+  def extract_signed_pdf(parsed_pdf)
+    file = Tempfile.new("signed_pdf")
+    begin
+      parsed_pdf.save(file.path)
+      File.binread(file.path)
+    ensure
+      file.close
+      file.unlink
+    end
+  end
+
+  def text_annotation(options = {})
+    width = options.fetch(:width, 200.0)
+    height = options.fetch(:height, 50.0)
+    size = options.fetch(:size, 8)
+
+    Origami::Annotation::AppearanceStream.new.tap do |annotation|
+      annotation.Type = Origami::Name.new("XObject")
+      annotation.Resources = Origami::Resources.new
+      annotation.Resources.ProcSet = [Origami::Name.new("Text")]
+      annotation.set_indirect(true)
+      annotation.Matrix = [1, 0, 0, 1, 0, 0]
+      annotation.BBox = [0, 0, width, height]
+      annotation.write(caption, x: size, y: (height / 2) - (size / 2), size: size)
+    end
+  end
+
+  def signature_annotation(options = {})
+    @signature_annotation ||= begin
+                                width = options.fetch(:width, 200.0)
+                                height = options.fetch(:height, 50.0)
+
+                                Origami::Annotation::Widget::Signature.new.tap do |annotation|
+                                  annotation.set_indirect(true)
+                                  annotation.Rect = Origami::Rectangle[llx: height, lly: width + height, urx: width + height, ury: width]
+                                  annotation.set_normal_appearance(text_annotation(width: width, height: height))
+                                end
+                              end
+  end
+
+  def certificate
+    @certificate ||= OpenSSL::X509::Certificate.new pdf_certificate
+  end
+
+  def private_key
+    @private_key ||= begin
+                       private_cert = File.binread der_pkcs12_path
+                       OpenSSL::PKCS12.new private_cert, signature_certificate_password
+                     end
+  end
+
+  def pdf_certificate
+    Rails.application.secrets.pdf_certificate
+  end
+
+  def der_pkcs12_path
+    Rails.application.secrets.der_pkcs12_path
+  end
+
+  def signature_certificate_password
+    Rails.application.secrets.signature_certificate_password
+  end
+
+  def caption
+    @caption ||= "Digitally Signed By: #{signedby}\nContact: #{contact}\nLocation: #{location}\nDate: #{date.iso8601}"
+  end
+
+  def signedby
+    "Ajuntament de Barcelona"
+  end
+
+  def location
+    "Barcelona"
+  end
+
+  def contact
+    "suport@decidim.barcelona"
+  end
+
+  def issuer
+    "Decidim"
+  end
+
+  def date
+    Time.current
+  end
+end
---
+++ spec/services/pdf_signature_barcelona_spec.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe PdfSignatureBarcelona do
+  let(:subject) { service }
+  let(:service) { described_class.new(params) }
+  let(:params) { { pdf: valid_pdf } }
+  let(:valid_pdf) { File.read("spec/fixtures/example_pdf.pdf") }
+
+  describe "#signed_pdf" do
+    let(:subject) { service.signed_pdf }
+
+    context "when a configuration variable is missing" do
+      before do
+        allow(service).to receive(:pdf_certificate).and_return(nil)
+      end
+
+      it "returns the same pdf received" do
+        expect(subject).to eq(valid_pdf)
+      end
+    end
+
+    context "when configuration variables are present and the certificates valid" do
+      let(:key) { OpenSSL::PKey::RSA.new(2048) }
+      let(:certificate) do
+        OpenSSL::X509::Certificate.new.tap do |cert|
+          cert.not_before = Time.current
+          cert.not_after = 10.years.from_now
+          cert.public_key = key.public_key
+          cert.sign(key, OpenSSL::Digest::SHA256.new)
+        end
+      end
+      let(:private_key) { double("private_key", key: key) }
+
+      before do
+        allow(service).to receive(:certificate).and_return(certificate)
+        allow(service).to receive(:private_key).and_return(private_key)
+        allow(service).to receive(:missing_configuration?).and_return(false)
+      end
+
+      context "with a valid pdf" do
+        it "returns a signed pdf with a page added" do
+          original_pdf = Origami::PDF.read("spec/fixtures/example_pdf.pdf")
+          output_pdf = Origami::PDF.read(StringIO.new(subject))
+          expect(original_pdf.pages.count).to eq(1)
+          expect(original_pdf.signed?).to be false
+          expect(output_pdf.pages.count).to eq(2)
+          expect(output_pdf.signed?).to be true
+        end
+      end
+
+      context "with an invalid pdf" do
+        let(:params) { { pdf: "wadus" } }
+
+        it { expect { subject }.to raise_error }
+      end
+    end
+  end
+end
