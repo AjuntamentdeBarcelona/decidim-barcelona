bc05d62829df59b466429b8291ad1b82a333a9ba
Marc Riera Casals <mrc2407@gmail.com>
2019-02-28 10:38:06 +0100

    Enforce permissions on the authorization

--- decidim-valid_auth/app/controllers/decidim/valid_auth/authorizations_controller.rb
+++ decidim-valid_auth/app/controllers/decidim/valid_auth/authorizations_controller.rb
@@ -3,7 +3,11 @@
 module Decidim
   module ValidAuth
     class AuthorizationsController < Decidim::Verifications::ApplicationController
+      before_action :load_authorization
+
       def new
+        enforce_permission_to :create, :authorization, authorization: @authorization
+
         redirect_to valid_link
       end
 
@@ -11,6 +15,13 @@ module Decidim
 
       def valid_link
         Rails.secrets.valid_auth_url + validate_authorization_url
+      end
+
+      def load_authorization
+        @authorization = Decidim::Authorization.find_or_initialize_by(
+          user: current_user,
+          name: "valid"
+        )
       end
     end
   end
