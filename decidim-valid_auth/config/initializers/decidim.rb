28fd2c767a123474dad12f900501dd9c7f8dfb4a
Marc Riera Casals <mrc2407@gmail.com>
2019-03-01 09:55:56 +0100

    Don't allow ValidAuth unless secret is present

--- decidim-valid_auth/config/initializers/decidim.rb
+++ decidim-valid_auth/config/initializers/decidim.rb
@@ -1,3 +1,5 @@
-Decidim::Verifications.register_workflow(:valid_auth) do |workflow|
-  workflow.engine = Decidim::ValidAuth::Engine
+if Rails.application.secrets.valid_auth_url
+  Decidim::Verifications.register_workflow(:valid_auth) do |workflow|
+    workflow.engine = Decidim::ValidAuth::Engine
+  end
 end
