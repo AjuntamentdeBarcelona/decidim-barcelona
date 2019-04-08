15995fe7555ee9dfad516997574249d41d192727
Oriol Gual <oriol@codegram.com>
2019-04-08 12:56:09 +0200

    Better migration

--- db/migrate/20190408103727_update_scopes.rb
+++ db/migrate/20190408103727_update_scopes.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 class UpdateScopes < ActiveRecord::Migration[5.2]
   def change
-    city_scope = Decidim::Scope.find(11)
+    city_scope = Decidim::Scope.where("name->>'ca' = 'Ciutat'").first
+    return unless city_scope
+
     districts = Decidim::Scope.where(parent_id: nil).select{|s| s.name["ca"] != "Ciutat" }
     districts.each {|district|
       district.parent = city_scope
