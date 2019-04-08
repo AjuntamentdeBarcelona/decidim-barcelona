86b75e94e2aa7a55a83a247abd607c50ece96a8f
Oriol Gual <oriol@codegram.com>
2019-04-08 14:50:12 +0200

    Fix old migration to fix review apps

--- db/migrate/20170616083641_add_uniqueness_to_name_and_document_number_to_user_groups.decidim.rb
+++ db/migrate/20170616083641_add_uniqueness_to_name_and_document_number_to_user_groups.decidim.rb
@@ -3,10 +3,12 @@
 
 class AddUniquenessToNameAndDocumentNumberToUserGroups < ActiveRecord::Migration[5.0]
   def change
-    Decidim::UserGroup.select(:document_number).group(:document_number).having("count(*) > 1").count.keys.each do |document_number|
-      Decidim::UserGroup.where(document_number: document_number).each_with_index do |user_group, index|
-        next if index == 0
-        user_group.update_attribute(:document_number, "#{document_number} (#{index})")
+    if Decidim::UserGroup.count > 1
+      Decidim::UserGroup.select(:document_number).group(:document_number).having("count(*) > 1").count.keys.each do |document_number|
+        Decidim::UserGroup.where(document_number: document_number).each_with_index do |user_group, index|
+          next if index == 0
+          user_group.update_attribute(:document_number, "#{document_number} (#{index})")
+        end
       end
     end
 
