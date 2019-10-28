10ca702ceecf67416b85b8b50e882368f7142fae
Oliver Valls <oliver.vh@coditramuntana.com>
2019-10-28 16:53:14 +0100

    Set amazon S3 region to Paris. (#261)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -18,8 +18,8 @@ if Rails.application.secrets.aws_access_key_id.present?
       provider:              'AWS',                                             # required
       aws_access_key_id:     Rails.application.secrets.aws_access_key_id,     # required
       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
-      region:                'eu-central-1',                                       # optional, defaults to 'us-east-1'
-      host:                  's3.eu-central-1.amazonaws.com',                                  # optional, defaults to nil
+      region:                'eu-west-3',                                       # optional, defaults to 'us-east-1'
+      host:                  's3.eu-west-3.amazonaws.com',                                  # optional, defaults to nil
     }
     config.fog_directory  = ENV.fetch("AWS_BUCKET_NAME", 'decidim-barcelona-new')              # required
     config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }    # optional, defaults to {}
