4cc32d60c55ccee83cf0c3016b5a92fd5c5391ac
Oliver Valls <oliver.vh@coditramuntana.com>
2019-12-05 12:57:14 +0100

    [CONFIG] Set default_signature_time_period_length = 60.

--- config/initializers/initiatives.rb
+++ config/initializers/initiatives.rb
@@ -3,4 +3,5 @@
 Decidim::Initiatives.configure do |config|
   config.face_to_face_voting_allowed = true
   config.online_voting_allowed = true
+  config.default_signature_time_period_length = 60
 end
