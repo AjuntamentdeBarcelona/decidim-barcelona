bf6c722b31cd5c13a94c44e056a79e3f4ddc476c
Oliver Valls <oliver.vh@coditramuntana.com>
2020-03-07 08:36:15 +0100

    Add migration to publish answers for already answered proposals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_114560) do
+ActiveRecord::Schema.define(version: 2020_03_06_221719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
