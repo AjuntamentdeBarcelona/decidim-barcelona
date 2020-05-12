d5a1437e585d0eec1f1ea7fe9ded0b7b0c180a0c
Ramon <ramon.ct@coditramuntana.com>
2020-05-12 15:39:04 +0200

    Override author event

---
+++ lib/decidim/events/author_event.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Events
+    # This module is used to be included in event classes inheriting from SimpleEvent
+    # whose resource has an author.
+    #
+    # It adds the author_name, author_nickname, author_path and author_url to the i18n interpolations.
+    module AuthorEvent
+      extend ActiveSupport::Concern
+
+      included do
+        i18n_attributes :author_name, :author_nickname, :author_path, :author_url
+
+        def author_nickname
+          author_presenter&.nickname.to_s
+        end
+
+        def author_name
+          author_presenter&.name.to_s
+        end
+
+        def author_path
+          author_presenter&.profile_path.to_s
+        end
+
+        def author_url
+byebug
+          author_presenter&.profile_url.to_s
+        end
+
+        def author_presenter
+          return unless author
+byebug
+          @author_presenter ||= "#{resource.class.parent}::OfficialAuthorPresenter".constantize.new
+        end
+
+        def author
+          return unless resource.respond_to?(:author)
+          return unless resource.author.is_a?(Decidim::UserBaseEntity)
+
+          resource.author
+        end
+      end
+    end
+  end
+end
