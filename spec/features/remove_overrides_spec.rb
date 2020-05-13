e7e54164a32f96586ba10a3c9106f69de7633d7c
Jesús Di Bari <jesus.db@coditramuntana.com>
2020-05-13 16:56:25 +0200

    Add test to remove override after decidim is at version 0.22

--- spec/features/remove_overrides_spec.rb
+++ spec/features/remove_overrides_spec.rb
@@ -7,4 +7,9 @@ describe "Overrides" do
     # remove lib/decidim/events/author_event.rb after https://github.com/decidim/decidim/issues/6091 is fixed
     expect(Decidim.version).to be < "0.22"
   end
+
+  scenario "remove override to fix incorrect organization time zone" do
+    # remove app/controllers/decidim/api/application_controller.rb after https://github.com/decidim/decidim/pull/6088 is merged
+    expect(Decidim.version).to be < "0.22"
+  end
 end
