4c154c028e8d86f41ca1abf753494b928ffeeb53
Ramon Costa <ramon.costa@gmail.com>
2020-05-18 17:43:08 +0200

    Change citizens initiative signature process to avoid gender check (#303)
    * Changes the way gender attribute is used
    
    * Fixes census authorizations handler rspec
    
    * DECIDIM-96 Cleans spaces
    
    * DECIDIM-96 Fix authorization date_of_birth format
    
    * DECIDIM-96 Add test to avoid checking for extras field when user makes an initiative signature
    
    Co-authored-by: Ramon <ramon.ct@coditramuntana.com>

--- app/services/census_authorization_handler.rb
+++ app/services/census_authorization_handler.rb
@@ -34,10 +34,12 @@ class CensusAuthorizationHandler < Decidim::AuthorizationHandler
   # it's created, and available though authorization.metadata
   def metadata
     super.merge(
-      date_of_birth: date_of_birth,
-      gender: gender,
+      date_of_birth: date_of_birth&.strftime("%Y-%m-%d"),
       postal_code: postal_code,
       scope: scope.name["ca"],
+      extras: {
+        gender: gender
+      }
     )
   end
 
