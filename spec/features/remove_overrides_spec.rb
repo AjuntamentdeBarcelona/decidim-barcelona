198ac4ab4c037af35aa3c62dde7d343b074497a4
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2020-09-09 08:32:13 +0200

    Remove unrequired overrides after upgrading to v0.22.0

--- spec/features/remove_overrides_spec.rb
+++ spec/features/remove_overrides_spec.rb
@@ -3,22 +3,4 @@
 require "rails_helper"
 
 describe "Overrides" do
-  scenario "remove override to fix created debates notifications" do
-    # remove lib/decidim/events/author_event.rb after https://github.com/decidim/decidim/issues/6091 is fixed
-    expect(Decidim.version).to be < "0.22"
-  end
-
-  scenario "remove override to fix incorrect organization time zone" do
-    # remove app/controllers/decidim/api/application_controller.rb after https://github.com/decidim/decidim/pull/6088 is merged
-    expect(Decidim.version).to be < "0.22"
-  end
-
-  scenario "remove detroy meeting related translations" do
-    # remove :ca and :es entries for:
-    # - decidim.meetings.actions.invalid_destroy.proposals_count.one
-    # - decidim.meetings.actions.invalid_destroy.proposals_count.other
-    # - decidim.meetings.admin.meetings.destroy.invalid.proposals_count.one
-    # - decidim.meetings.admin.meetings.destroy.invalid.proposals_count.other
-    expect(Decidim.version).to be < "0.22"
-  end
 end
