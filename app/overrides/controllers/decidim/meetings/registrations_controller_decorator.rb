500e25f09b5d2303428bcbcb20939f0aaf8da5cd
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2020-10-23 18:34:25 +0200

    Use full namespaces inside controller decorator

--- app/overrides/controllers/decidim/meetings/registrations_controller_decorator.rb
+++ app/overrides/controllers/decidim/meetings/registrations_controller_decorator.rb
@@ -4,9 +4,9 @@ Decidim::Meetings::RegistrationsController.class_eval do
   def create
     enforce_permission_to :register, :meeting, meeting: meeting
 
-    @form = JoinMeetingForm.from_params(params)
+    @form = Decidim::Meetings::JoinMeetingForm.from_params(params)
 
-    JoinMeeting.call(meeting, current_user, @form) do
+    Decidim::Meetings::JoinMeeting.call(meeting, current_user, @form) do
       on(:ok) do
         flash[:notice] = I18n.t("registrations.create.success", scope: "decidim.meetings")
         redirect_after_path
