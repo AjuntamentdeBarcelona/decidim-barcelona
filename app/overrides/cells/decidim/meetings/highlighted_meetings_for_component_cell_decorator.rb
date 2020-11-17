ef570798c880de6b88eb290b6009f6515a55876c
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2020-11-17 08:01:13 +0100

    Remove deprecated override

--- app/overrides/cells/decidim/meetings/highlighted_meetings_for_component_cell_decorator.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-Decidim::Meetings::HighlightedMeetingsForComponentCell.class_eval do
-  def meetings
-    @meetings ||= Decidim::Meetings::Meeting.where(component: model).visible_meeting_for(current_user)
-  end
-end
