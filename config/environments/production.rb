11fc1d09a961610471c1af938ddd6e08d0bf4f05
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2020-11-17 08:25:45 +0100

    Replace :dalli_store for Rails' official :mem_cache_store instead
    DEPRECATION: :dalli_store will be removed in Dalli 3.0.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,7 +101,7 @@ Rails.application.configure do
   end
 
   if ENV["MEMCACHEDCLOUD_SERVERS"]
-    config.cache_store = :dalli_store, ENV["MEMCACHEDCLOUD_SERVERS"].split(","), {
+    config.cache_store = :mem_cache_store, ENV["MEMCACHEDCLOUD_SERVERS"].split(","), {
       username: ENV["MEMCACHEDCLOUD_USERNAME"], password: ENV["MEMCACHEDCLOUD_PASSWORD"]
     }
   end
