0ff72823228835392d4d51c5bd3b191f48a5db64
Marc Riera Casals <mrc2407@gmail.com>
2021-03-08 10:00:43 +0100

    Remove overrides

--- spec/features/remove_overrides_spec.rb
+++ spec/features/remove_overrides_spec.rb
@@ -3,8 +3,4 @@
 require "rails_helper"
 
 describe "Overrides" do
-  it "remove consistent sorting of proposal attachments" do
-    # remove app/overrides/cells/decidim/proposals/proposal_m_cell_decorator.rb
-    expect(Decidim.version).to be < "0.24"
-  end
 end
