db1ff0f194a91ee7e6253093b57828e5df38d005
Marc Riera Casals <mrc2407@gmail.com>
2021-03-19 09:21:14 +0100

    Update to latest decidim 0.23

---
+++ db/migrate/20210319081722_fix_answered_proposals_after_copy.decidim_proposals.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+# This migration comes from decidim_proposals (originally 20210127115628)
+
+class FixAnsweredProposalsAfterCopy < ActiveRecord::Migration[5.2]
+  def change
+    proposals_after_copy = Decidim::ResourceLink.where(from_type: "Decidim::Proposals::Proposal").pluck(:from_id)
+
+    result = Decidim::Proposals::Proposal.where.not(state_published_at: nil).where(state: nil, id: proposals_after_copy)
+
+    result.find_each do |proposal|
+      proposal.state_published_at = nil
+      proposal.save!
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_094434) do
+ActiveRecord::Schema.define(version: 2021_03_19_081723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
