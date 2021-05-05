e5129a23d830d92a04c00f26a3fa901dac158447
Marc Riera <mrc2407@gmail.com>
2021-05-05 16:38:11 +0200

    Add test to ensure upgrade will be fine (#362)

--- spec/features/remove_overrides_spec.rb
+++ spec/features/remove_overrides_spec.rb
@@ -7,4 +7,13 @@ describe "Overrides" do
     # remove app/overrides/cells/decidim/proposals/proposal_m_cell_decorator.rb
     expect(Decidim.version).to be < "0.24"
   end
+
+  it "fix how the stats module retrieves users" do
+    # check Performers in decidim-stats.
+    #
+    # They no longer can query Authorizations directly because the  metadata`
+    # field is now encrypted. We need to load of all them and filter in Ruby
+    # side.
+    expect(Decidim.version).to be < "0.24"
+  end
 end
