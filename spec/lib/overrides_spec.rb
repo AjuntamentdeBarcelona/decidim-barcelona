5064201ad7effd1e463fd8269daa914d67041298
Sandra Martos <sandra.martos@nazaries.com>
2023-01-09 16:50:03 +0100

    text of responsible declaration in initiatives signature fixed

--- spec/lib/overrides_spec.rb
+++ spec/lib/overrides_spec.rb
@@ -64,7 +64,8 @@ checksums = [
     package: "decidim-initiatives",
     files: {
       "/app/cells/decidim/initiatives/initiative_m_cell.rb" => "a20b707d0533dd8883b0bdbf8bc0b2c0",
-      "/app/views/layouts/decidim/_initiative_header_steps.html.erb" => "f1bcd3e7c406a2263d49d0f341930bfc"
+      "/app/views/layouts/decidim/_initiative_header_steps.html.erb" => "f1bcd3e7c406a2263d49d0f341930bfc",
+      "/app/views/decidim/initiatives/initiative_signatures/fill_personal_data.html.erb" => "2c3068724ed2986f62bd13994960f39e"
     }
   },
   {
