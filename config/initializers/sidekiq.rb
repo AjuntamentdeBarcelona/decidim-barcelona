7c16312943a2c73e1bfbcec0a020ac3d398ea4ad
Fran Bolívar <francisco.bolivar.lupianez@gmail.com>
2023-02-08 13:30:46 +0100

    Fix Redis TLS configuration (#451)

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+if Rails.env.production?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+  end
+end
