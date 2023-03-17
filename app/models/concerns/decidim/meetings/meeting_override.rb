096d35a076c2d55407f293771c80e89181310474
Alejandro Puerta <alejandro.puerta@nazaries.com>
2023-03-17 10:04:54 +0100

    fix: meeting_override.rb rubocop fixes

--- app/models/concerns/decidim/meetings/meeting_override.rb
+++ app/models/concerns/decidim/meetings/meeting_override.rb
@@ -13,7 +13,8 @@ module Decidim
           roles_table = "#{table_name.singularize}_user_roles"
           return unless ActiveRecord::Base.connection.table_exists?(roles_table)
 
-          query = "SELECT decidim_user_id FROM #{roles_table} WHERE decidim_user_id = #{user.id} AND #{table_name.singularize}_id = #{component.participatory_space.id} AND role = 'admin'"
+          query = "SELECT decidim_user_id FROM #{roles_table} WHERE decidim_user_id = #{user.id}
+                   AND #{table_name.singularize}_id = #{component.participatory_space.id} AND role = 'admin'"
           result = ActiveRecord::Base.connection.execute(query)
           result.first
         end
