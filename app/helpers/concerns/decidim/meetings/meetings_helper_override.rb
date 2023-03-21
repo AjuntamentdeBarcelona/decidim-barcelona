c0abd15e0de77219bc3576015172f9d1f18c193b
Alejandro Puerta <alejandro.puerta@nazaries.com>
2023-03-21 13:40:35 +0100

    fix: now it takes into account sub items inside the parent agenda.

--- app/helpers/concerns/decidim/meetings/meetings_helper_override.rb
+++ app/helpers/concerns/decidim/meetings/meetings_helper_override.rb
@@ -6,8 +6,21 @@ module Decidim
       extend ActiveSupport::Concern
 
       included do
-        def search_for_zero_duration_events(agendas_times)
-          agendas_times.any? { |agenda_time| agenda_time[:start_time] - agenda_time[:end_time] == 0 }
+        # Public: Check if any agenda time has a 0 duration with a depth of two levels
+        # TODO Implementation as a recursive method so that it takes into account higher levels of depth
+        #
+        # agendas - Agenda items for the meeting with zero depth
+        # meeting - The meeting
+        def any_zero_duration_event?(agendas, meeting)
+          agenda_items_times = calculate_start_and_end_time_of_agenda_items(agendas, meeting)
+          agenda_items_times.any? { |agenda_time| agenda_time[:start_time] - agenda_time[:end_time] == 0 } ||
+            agendas.map.with_index do |agenda, index|
+              next unless agenda.agenda_item_children.presence
+
+              parent_start_time = agenda_items_times[index][:start_time]
+              agenda_items_children_times = calculate_start_and_end_time_of_agenda_items(agenda.agenda_item_children, meeting, parent_start_time)
+              agenda_items_children_times.any? { |agenda_children| agenda_children[:start_time] - agenda_children[:end_time] == 0 }
+            end.include?(true)
         end
       end
     end
