cd92b849f1e3f81c2de2c686eea04435343655a8
Alejandro Puerta <alejandro.puerta@nazaries.com>
2023-03-21 13:46:36 +0100

    fix: rubocop fixes

--- app/helpers/concerns/decidim/meetings/meetings_helper_override.rb
+++ app/helpers/concerns/decidim/meetings/meetings_helper_override.rb
@@ -13,13 +13,13 @@ module Decidim
         # meeting - The meeting
         def any_zero_duration_event?(agendas, meeting)
           agenda_items_times = calculate_start_and_end_time_of_agenda_items(agendas, meeting)
-          agenda_items_times.any? { |agenda_time| agenda_time[:start_time] - agenda_time[:end_time] == 0 } ||
+          agenda_items_times.any? { |agenda_time| (agenda_time[:start_time] - agenda_time[:end_time]).zero? } ||
             agendas.map.with_index do |agenda, index|
               next unless agenda.agenda_item_children.presence
 
               parent_start_time = agenda_items_times[index][:start_time]
               agenda_items_children_times = calculate_start_and_end_time_of_agenda_items(agenda.agenda_item_children, meeting, parent_start_time)
-              agenda_items_children_times.any? { |agenda_children| agenda_children[:start_time] - agenda_children[:end_time] == 0 }
+              agenda_items_children_times.any? { |agenda_children| (agenda_children[:start_time] - agenda_children[:end_time]).zero? }
             end.include?(true)
         end
       end
