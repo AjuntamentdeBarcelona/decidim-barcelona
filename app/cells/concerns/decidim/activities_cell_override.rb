88ebdf45cfc447485f143ab36851a259b31db9ab
Francisco Bolívar <francisco.bolivar@nazaries.com>
2023-07-17 11:03:55 +0200

    Fix overrides spec

--- app/cells/concerns/decidim/activities_cell_override.rb
+++ app/cells/concerns/decidim/activities_cell_override.rb
@@ -7,7 +7,17 @@ module Decidim
     included do
       def activities
         @activities ||= last_activities.select do |activity|
-          activity.visible_for?(current_user) && (!activity.respond_to?(:component) || activity.component.published?)
+          !activity.respond_to?(:component) || activity.component.published?
+        end
+      end
+
+      def last_activities
+        @last_activities ||= model.map do |activity|
+          activity.organization_lazy
+          activity.resource_lazy
+          activity.participatory_space_lazy
+          activity.component_lazy
+          activity
         end
       end
     end
