d75fdc6be7d4cf93e6172a1d44dc9b5ef8c5eae5
Francisco Bolívar <francisco.bolivar@nazaries.com>
2023-08-09 15:43:22 +0200

    Fix legacy routing redirections

--- spec/routing/redirections_spec.rb
+++ spec/routing/redirections_spec.rb
@@ -7,6 +7,8 @@ require "decidim/assemblies/test/factories"
 require "decidim/participatory_processes/test/factories"
 require "decidim/proposals/test/factories"
 require "decidim/meetings/test/factories"
+require "decidim/debates/test/factories"
+require "decidim/accountability/test/factories"
 
 describe "routing redirections", type: :request do
   let(:organization) { create(:organization, host: "decidim.barcelona", available_authorizations: authorizations) }
@@ -15,6 +17,100 @@ describe "routing redirections", type: :request do
       "dummy_authorization_handler" => { "allow_ephemeral_participation" => true },
       "another_dummy_authorization_handler" => { "allow_ephemeral_participation" => false }
     }
+  end
+
+  describe "debates" do
+    let(:participatory_space) { create(:participatory_process, organization: organization, slug: "test-process") }
+    let(:component) { create(:debates_component, participatory_space: participatory_space) }
+    let!(:debate) { create(:debate, component: component, extra: { slug: "test-debate" }) }
+
+    context "with the right host" do
+      before do
+        host! organization.host
+      end
+
+      it "redirects top-level debates" do
+        expect(get("/debates/test-debate"))
+          .to redirect_to("/processes/#{participatory_space.slug}/f/#{component.id}/debates/#{debate.id}")
+      end
+
+      it "redirects debates inside the old structure without step ID" do
+        expect(get("/test-process/debates/test-debate"))
+          .to redirect_to("/processes/#{participatory_space.slug}/f/#{component.id}/debates/#{debate.id}")
+      end
+
+      it "redirects debates inside the old structure without step ID using debate id" do
+        expect(get("/test-process/debates/#{debate.id}"))
+          .to redirect_to("/processes/#{participatory_space.slug}/f/#{component.id}/debates/#{debate.id}")
+      end
+
+      it "redirects debates inside the old step structure" do
+        expect(get("/test-process/123/debates/test-debate"))
+          .to redirect_to("/processes/#{participatory_space.slug}/f/#{component.id}/debates/#{debate.id}")
+      end
+
+      it "redirects debates inside the old step structure using debate id" do
+        expect(get("/test-process/123/debates/#{debate.id}"))
+          .to redirect_to("/processes/#{participatory_space.slug}/f/#{component.id}/debates/#{debate.id}")
+      end
+
+      it "redirects index paths inside the old step structure" do
+        expect(get("/test-process/123/debates/"))
+          .to redirect_to("/processes/#{participatory_space.slug}/f/#{component.id}")
+      end
+    end
+
+    context "with the wrong host" do
+      before do
+        host! "foo.bar"
+      end
+
+      it "doesn't redirect" do
+        expect { get("/debates/test-debate") }.to raise_error(ActionController::RoutingError)
+      end
+    end
+  end
+
+  describe "accountability" do
+    let(:participatory_space) { create(:participatory_process, organization: organization, slug: "test-process") }
+    let(:component) { create(:accountability_component, participatory_space: participatory_space) }
+    let!(:result) { create(:result, component: component) }
+
+    context "with the right host" do
+      before do
+        host! organization.host
+      end
+
+      it "redirects top-level accountability result" do
+        expect(get("/action_plans/#{result.id}"))
+          .to redirect_to("/processes/#{participatory_space.slug}/f/#{component.id}/results/#{result.id}")
+      end
+
+      it "redirects accountability results inside the old structure without step ID" do
+        expect(get("/test-process/action_plans/#{result.id}"))
+          .to redirect_to("/processes/#{participatory_space.slug}/f/#{component.id}/results/#{result.id}")
+      end
+
+      it "redirects accountability results inside the old step structure" do
+        expect(get("/test-process/123/action_plans/#{result.id}"))
+          .to redirect_to("/processes/#{participatory_space.slug}/f/#{component.id}/results/#{result.id}")
+      end
+
+      it "redirects index paths inside the old step structure" do
+        expect(get("/test-process/123/action_plans/"))
+          .to redirect_to("/processes/#{participatory_space.slug}/f/#{component.id}")
+      end
+    end
+
+    context "with the wrong host" do
+      before do
+        host! "foo.bar"
+      end
+
+      it "doesn't redirect" do
+        expect { get("/action_plans/test-result") }.to raise_error(ActionController::RoutingError)
+      end
+    end
   end
 
   describe "proposals" do
@@ -29,22 +125,22 @@ describe "routing redirections", type: :request do
 
       it "redirects top-level proposals" do
         expect(get("/proposals/test-proposal"))
-          .to redirect_to("/processes/#{participatory_process.id}/f/#{component.id}/proposals/#{proposal.id}")
+          .to redirect_to("/processes/#{participatory_process.slug}/f/#{component.id}/proposals/#{proposal.id}")
       end
 
       it "redirects proposals inside the old structure without step ID" do
         expect(get("/test-process/proposals/test-proposal"))
-          .to redirect_to("/processes/#{participatory_process.id}/f/#{component.id}/proposals/#{proposal.id}")
+          .to redirect_to("/processes/#{participatory_process.slug}/f/#{component.id}/proposals/#{proposal.id}")
       end
 
       it "redirects proposals inside the old step structure" do
         expect(get("/test-process/123/proposals/test-proposal"))
-          .to redirect_to("/processes/#{participatory_process.id}/f/#{component.id}/proposals/#{proposal.id}")
+          .to redirect_to("/processes/#{participatory_process.slug}/f/#{component.id}/proposals/#{proposal.id}")
       end
 
       it "redirects index paths inside the old step structure" do
         expect(get("/test-process/123/proposals/"))
-          .to redirect_to("/processes/#{participatory_process.id}/f/#{component.id}")
+          .to redirect_to("/processes/#{participatory_process.slug}/f/#{component.id}")
       end
     end
 
