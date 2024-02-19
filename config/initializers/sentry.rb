00ccf75ab2bfd8da495285108a7148d81d915fde
Francisco Bolívar <francisco.bolivar@nazaries.com>
2024-02-19 13:48:15 +0100

    Set up profiling

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,6 +4,7 @@ if Rails.application.secrets.sentry_enabled
   Sentry.init do |config|
     config.dsn = ENV.fetch("SENTRY_DSN", nil)
     config.traces_sample_rate = ENV.fetch("SENTRY_TRACES_SAMPLE_RATE", "0.5").to_f
+    config.profiles_sample_rate = ENV.fetch("SENTRY_PROFILES_SAMPLE_RATE", "0.5").to_f
     config.environment = ENV["HEROKU_APP_NAME"].presence || "production"
 
     config.breadcrumbs_logger = [:active_support_logger]
