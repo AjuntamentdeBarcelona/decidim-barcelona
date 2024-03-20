da396da3dcd95465574a1189a8d9690f281e1d1a
Francisco Bolívar <francisco.bolivar@nazaries.com>
2024-03-20 08:37:52 +0100

    Update questionnaire user answers override

--- config/initializers/decidim_overrides.rb
+++ config/initializers/decidim_overrides.rb
@@ -16,5 +16,5 @@ Rails.application.config.to_prepare do
   Decidim::ActivitiesCell.include(Decidim::ActivitiesCellOverride)
   Decidim::UserProfileCell.include(Decidim::UserProfileCellOverride)
   Decidim::Proposals::ProposalPresenter.include(Decidim::Proposals::ProposalPresenterOverride)
-  # Decidim::Forms::QuestionnaireUserAnswers.include(Decidim::Forms::QuestionnaireUserAnswersOverride)
+  Decidim::Forms::QuestionnaireUserAnswers.include(Decidim::Forms::QuestionnaireUserAnswersOverride)
 end
