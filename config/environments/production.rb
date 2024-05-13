e463e4a9f88fb32a4b052f08cee6679583abbed3
Francisco Bolívar <francisco.bolivar@nazaries.com>
2024-05-13 16:03:14 +0200

    Use null store as cache store for action controller

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,8 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
+  config.action_controller.cache_store = :null_store
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
