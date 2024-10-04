48124da4ffb2ce75afc7b113f3ab900143eceb47
Maxim Colls <collsmaxim@gmail.com>
2024-10-04 11:58:11 +0200

    Add verification data export page in stats module

--- decidim-stats/lib/decidim/stats/engine.rb
+++ decidim-stats/lib/decidim/stats/engine.rb
@@ -4,6 +4,19 @@ module Decidim
   module Stats
     class Engine < ::Rails::Engine
       isolate_namespace Decidim::Stats
+
+      routes do
+        resources :authorization_exports, only: [:index, :create]
+      end
+
+      initializer "decidim_stats.admin_menus" do
+        Decidim.menu :admin_user_menu do |menu|
+          menu.add_item :authorization_exports,
+                        I18n.t("authorization_exports.menu", scope: "decidim.stats"),
+                        decidim_stats.authorization_exports_path,
+                        active: is_active_link?(decidim_stats.authorization_exports_path)
+        end
+      end
     end
   end
 end
