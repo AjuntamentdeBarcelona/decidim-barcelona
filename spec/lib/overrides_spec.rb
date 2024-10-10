7f18869c57048ee41a73bed865bb01f26b81b9c9
Harrison Hassig <harrisonhassig@gmail.com>
2024-10-10 18:23:41 +0200

    Add a language selector in the header navigation (#551)
    * Desktop menu function and style
    
    * Overrides checksum matching for rspec
    
    * Header language selector for mobile view
    
    * Lint stylesheets
    
    * Stylesheet unit fix
    
    * Spacing in _main.html.erb
    
    * Cleaned up the language selector [full language name, down-arrow icon]
    
    * Mobile language menu fix
    
    * Restore Gemfile.lock
    
    * Fix style and tests specs as per suggestions
    
    * Style lint fix
    
    * Expected result of header/main override spec
    
    * typo and dropdown menu fully visible

--- spec/lib/overrides_spec.rb
+++ spec/lib/overrides_spec.rb
@@ -27,7 +27,8 @@ checksums = [
       "/app/cells/decidim/activities_cell.rb" => "dd17416a8d1efe7f26b1e759613e4db4",
       "/app/cells/decidim/user_profile_cell.rb" => "71216a17c7eea8dd8ebc1ea3907efaef",
       "/lib/decidim/view_model.rb" => "141f2ea7b4c8bb35bd3966882780c225",
-      "/lib/decidim/search_resource_fields_mapper.rb" => "ff2cc476eb72c2942cf2e69ae21b84fa"
+      "/lib/decidim/search_resource_fields_mapper.rb" => "ff2cc476eb72c2942cf2e69ae21b84fa",
+      "/app/views/layouts/decidim/header/_main.html.erb" => "a6496ec11e073062743a927ee3c8bd3c"
     }
   },
   {
