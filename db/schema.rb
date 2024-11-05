21f6eadcd30cf897f4abd32f4e826e83db16d01d
Fran Bolívar <francisco.bolivar.lupianez@gmail.com>
2024-11-05 09:39:03 +0100

    Bump decidim version from `6e50c98` to `739d2cc` (#570)
    * Bump decidim version from 6e50c98a52 to 09d0799e80
    
    * Add and install migration
    
    * Adapt overrides
    
    * Ignore CC alert
    
    * Bump decidim from 09d0799 to 739d2cc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_15_093725) do
+ActiveRecord::Schema.define(version: 2024_10_31_144530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
@@ -1880,6 +1880,7 @@ ActiveRecord::Schema.define(version: 2024_10_15_093725) do
     t.datetime "digest_sent_at"
     t.datetime "password_updated_at"
     t.string "previous_passwords", default: [], array: true
+    t.boolean "email_on_assigned_proposals", default: true
     t.index ["confirmation_token"], name: "index_decidim_users_on_confirmation_token", unique: true
     t.index ["decidim_organization_id"], name: "index_decidim_users_on_decidim_organization_id"
     t.index ["email", "decidim_organization_id"], name: "index_decidim_users_on_email_and_decidim_organization_id", unique: true, where: "((deleted_at IS NULL) AND (managed = false) AND ((type)::text = 'Decidim::User'::text))"
