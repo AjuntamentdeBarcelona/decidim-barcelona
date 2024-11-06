f5a49bd1a7313611116bb0135d2a2092db9eb237
Francisco Bolívar <francisco.bolivar@nazaries.com>
2024-11-06 17:48:15 +0100

    Apply suggestions

---
+++ app/controllers/concerns/decidim/pages_controller_override.rb
@@ -0,0 +1,15 @@
+module Decidim
+  module PagesControllerOverride
+    extend ActiveSupport::Concern
+
+    included do
+      before_action :accessibility_redirect
+
+      def accessibility_redirect
+        return if params[:id] != "accessibilitat"
+
+        redirect_to "https://ajuntament.barcelona.cat/declaracio-accessibilitat/#{I18n.locale}/decidim-barcelona"
+      end
+    end
+  end
+end
