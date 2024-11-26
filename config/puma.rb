fa0cf24f6dd9385fbdacb4fc30b71dcb94a193a1
Francisco Bolívar <francisco.bolivar@nazaries.com>
2024-11-26 12:41:55 +0100

    Uninstall puma_worker_killer

--- config/puma.rb
+++ config/puma.rb
@@ -9,9 +9,3 @@ preload_app!
 
 port ENV.fetch("PORT", nil) || 3000
 environment ENV.fetch("RACK_ENV", nil) || "development"
-
-before_fork do
-  require "puma_worker_killer"
-
-  PumaWorkerKiller.enable_rolling_restart
-end
