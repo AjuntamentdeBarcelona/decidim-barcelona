176c08067cdb4d68dc8ad1795ee1593a6e9d014b
Francisco Bolívar <francisco.bolivar@nazaries.com>
2025-02-20 15:51:46 +0100

    Revert "Update config/application.rb"
    This reverts commit b03ed6ae9e0aa74308e2c0fe6e49bd687cf690a4.

--- config/application.rb
+++ config/application.rb
@@ -14,15 +14,9 @@ Bundler.require(*Rails.groups)
 
 module DecidimBarcelona
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.autoloader = :zeitwerk
   end
 end
