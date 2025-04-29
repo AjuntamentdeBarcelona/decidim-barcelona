49e85ba88e0b4ad6da7c2c2e18e1034d6cb29f3e
Francisco Bolívar <francisco.bolivar@nazaries.com>
2025-04-29 16:53:57 +0200

    Fix broken cells

--- app/cells/census_authorization_metadata_cell.rb
+++ app/cells/census_authorization_metadata_cell.rb
@@ -1,5 +1,26 @@
 # frozen_string_literal: true
 
 # This cell is to render the census authorization metadata in the renew modal
-class CensusAuthorizationMetadataCell < CensusAuthorizationMetadataCell
+class CensusAuthorizationMetadataCell < Decidim::Verifications::AuthorizationMetadataCell
+  private
+
+  def metadata_key(data)
+    data[0] = "scope_id" if data.first == "scope"
+    super
+  end
+
+  def metadata_value(data)
+    value = data.second
+    value = l(value.to_date, format: :decidim_short) if data.first == "date_of_birth"
+
+    if %w(man non_binary woman).include?(value)
+      t(value, scope: "#{authorization_name}.form.genders")
+    else
+      value
+    end
+  end
+
+  def authorization_name
+    @authorization_name ||= model.name.chomp("_handler")
+  end
 end
