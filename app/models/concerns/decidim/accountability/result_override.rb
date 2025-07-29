feed2e87e1f6b62cd2818fe53bf5e971a9b01f9e
Fran Bolívar <francisco.bolivar@nazaries.com>
2025-07-29 13:56:17 +0200

    Avoid having empty external ids on accountability results

--- app/models/concerns/decidim/accountability/result_override.rb
+++ app/models/concerns/decidim/accountability/result_override.rb
@@ -6,8 +6,16 @@ module Decidim
       extend ActiveSupport::Concern
 
       included do
+        before_validation :remove_external_id, if: -> { external_id.blank? }
+
         def self.not_computable_results
           [11_098, 11_131]
+        end
+
+        private
+
+        def remove_external_id
+          self.external_id = nil
         end
       end
     end
--- spec/factories.rb
+++ spec/factories.rb
@@ -3,6 +3,7 @@
 require "decidim/core/test/factories"
 require "decidim/assemblies/test/factories"
 require "decidim/initiatives/test/factories"
+require "decidim/accountability/test/factories"
 
 FactoryBot.define do
   sequence(:valid_jwt) do |_n|
