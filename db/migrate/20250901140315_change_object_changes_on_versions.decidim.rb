8e4cf058c2de0b882bf0959ef5b571c6de152ccf
Alejandro Puerta <alejandro.puerta@nazaries.com>
2025-09-18 17:53:20 +0200

    Comment out PaperTrail migration logic in version change migration

--- db/migrate/20250901140315_change_object_changes_on_versions.decidim.rb
+++ db/migrate/20250901140315_change_object_changes_on_versions.decidim.rb
@@ -6,12 +6,12 @@ class ChangeObjectChangesOnVersions < ActiveRecord::Migration[6.1]
     rename_column :versions, :object_changes, :old_object_changes
     add_column :versions, :object_changes, :jsonb # or :json
 
-    PaperTrail::Version.reset_column_information
+    # PaperTrail::Version.reset_column_information
 
-    PaperTrail::Version.where.not(old_object_changes: nil).find_each do |version|
+    # PaperTrail::Version.where.not(old_object_changes: nil).find_each do |version|
       # we set a time interval to ensure that deployment has succeeded
-      Decidim::Migrate::PaperTrailJob.set(wait: 10.minutes).perform_later(version.id)
-    end
+    # Decidim::Migrate::PaperTrailJob.set(wait: 10.minutes).perform_later(version.id)
+    # end
   end
 
   def down
