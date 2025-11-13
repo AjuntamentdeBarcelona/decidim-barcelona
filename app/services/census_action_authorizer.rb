138578c6ed0dd83b952af8bda87af55d4713390a
Fran Bolívar <francisco.bolivar@nazaries.com>
2025-11-13 17:24:40 +0100

    Fix behavior

--- app/services/census_action_authorizer.rb
+++ app/services/census_action_authorizer.rb
@@ -4,24 +4,42 @@
 class CensusActionAuthorizer < Decidim::Verifications::DefaultActionAuthorizer
   BASE_OPTION_KEY = "scope_code"
 
-  protected
+  def missing_fields
+    super - options.keys
+  end
 
-  def unmatched_fields
-    scope_valid? ? [] : [BASE_OPTION_KEY]
+  def authorize
+    status_code, data = *super
+
+    extra_explanations = []
+    if allowed_scopes.present?
+      status_code = :unauthorized if status_code == :ok && !scope_valid?
+
+      unless scope_valid?
+        i18n_scope = "decidim.authorization_handlers.census_authorization_handler"
+        extra_explanations << { key: "extra_explanation",
+                                params: { scope: i18n_scope,
+                                          count: allowed_scopes.count,
+                                          scopes: allowed_scopes.map { |id| I18n.t("#{i18n_scope}.fields.scope_code_#{id}") }.join(", ") } }
+      end
+    end
+
+    data[:extra_explanation] = extra_explanations if extra_explanations.any?
+
+    [status_code, data]
   end
 
   private
 
+  def allowed_scopes
+    @allowed_scopes = options.keys
+                             .select { |k| k.to_s.match?(/^#{BASE_OPTION_KEY}_/) }
+                             .select { |k| options[k] == "1" }
+                             .map { |k| k.to_s.gsub("#{BASE_OPTION_KEY}_", "") }
+  end
+
   def scope_valid?
-    scope_codes = options.keys
-                         .select { |k| k.to_s.match?(/^#{BASE_OPTION_KEY}_/) }
-                         .select { |k| options[k] == "1" }
-                         .map { |k| k.to_s.gsub("#{BASE_OPTION_KEY}_", "") }
-
-    # No district restrictions set, all scopes are valid
-    return true if scope_codes.empty?
-
-    authorized_code = authorization.metadata[BASE_OPTION_KEY]
-    authorized_code.present? && scope_codes.include?(authorized_code.to_s)
+    scope = authorization.metadata[BASE_OPTION_KEY]
+    scope.present? && allowed_scopes.include?(scope.to_s)
   end
 end
