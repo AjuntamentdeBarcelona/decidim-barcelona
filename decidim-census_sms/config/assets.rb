3011f2c906fd771332e544256213de25e44f95ba
Fran Bolívar <francisco.bolivar@nazaries.com>
2026-01-20 10:42:51 +0100

    Remove deprecation warning

--- decidim-census_sms/config/assets.rb
+++ decidim-census_sms/config/assets.rb
@@ -12,29 +12,29 @@ base_path = File.expand_path("..", __dir__)
 # in your JavaScript entrypoints (or other JavaScript files within Decidim)
 # using `import "src/decidim/foo"` after you have registered the additional path
 # as follows.
-Decidim::Webpacker.register_path("#{base_path}/app/packs")
+Decidim::Shakapacker.register_path("#{base_path}/app/packs")
 
 # Register the entrypoints for your module. These entrypoints can be included
 # within your application using `javascript_pack_tag` and if you include any
 # SCSS files within the entrypoints, they become available for inclusion using
 # `stylesheet_pack_tag`.
-Decidim::Webpacker.register_entrypoints(
+Decidim::Shakapacker.register_entrypoints(
   decidim_census_sms: "#{base_path}/app/packs/entrypoints/decidim_census_sms.scss"
 )
 
 # If you want to import some extra SCSS files in the Decidim main SCSS file
 # without adding any extra stylesheet inclusion tags, you can use the following
 # method to register the stylesheet import for the main application.
-# Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/barcelona/barcelona")
+# Decidim::Shakapacker.register_stylesheet_import("stylesheets/decidim/barcelona/barcelona")
 
 # If you want to do the same but include the SCSS file for the admin panel's
 # main SCSS file, you can use the following method.
-# Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/barcelona/admin", group: :admin)
+# Decidim::Shakapacker.register_stylesheet_import("stylesheets/decidim/barcelona/admin", group: :admin)
 
 # If you want to override some SCSS variables/settings for Foundation from the
 # module, you can add the following registered import.
-# Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/foo/settings", type: :settings)
+# Decidim::Shakapacker.register_stylesheet_import("stylesheets/decidim/foo/settings", type: :settings)
 
 # If you want to do the same but override the SCSS variables of the admin
 # panel's styles, you can use the following method.
-# Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/foo/admin_settings", type: :settings, group: :admin)
+# Decidim::Shakapacker.register_stylesheet_import("stylesheets/decidim/foo/admin_settings", type: :settings, group: :admin)
