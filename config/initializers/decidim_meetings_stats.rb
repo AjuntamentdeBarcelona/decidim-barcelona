84e9f3a93e955ac37e618a8387d57f1b8fbec79c
Fran Bolívar <francisco.bolivar@nazaries.com>
2026-07-20 17:20:41 +0200

    Show attendees statistic for all the meeting types

--- config/initializers/decidim_meetings_stats.rb
+++ config/initializers/decidim_meetings_stats.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 Decidim.find_component_manifest(:meetings).stats.register(
-  :in_person_attendees_count,
+  :meetings_attendees_count,
   priority: Decidim::StatsRegistry::MEDIUM_PRIORITY,
   admin: false,
   icon_name: "group-line",
-  tooltip_key: "in_person_attendees_count_tooltip"
+  tooltip_key: "meetings_attendees_count_tooltip"
 ) do |components, _start_at, _end_at|
   Decidim::Meetings::Meeting.closed.not_hidden.published
                             .where(component: components, closing_visible: true)
-                            .in_person
                             .sum(:attendees_count)
 end
